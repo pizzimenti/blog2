f6e35f0b3e789aa8414c05997d7a4f202f40d6f3
Brad Pizzimenti and Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-03-31 17:30:50 -0700

    add project diagrams

--- app/components/blog-tile.js
+++ app/components/blog-tile.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateFlag: false,
   actions:{
     sendDelete(blog){
       this.sendAction("sendDelete", blog);
+    },
+    setUpdateFlag() {
+    this.set("updateFlag", !this.updateFlag);
+    console.log("update flag", this.updateFlag);
     }
   }
 });
